10e62e3a440562c65bf7fd5bbf8e5b58c9db83a5
prince nyarko <nyankonton122@gmail.com>
2024-09-03 01:42:11 -0300

    was geting 6 dates

--- src/utils/getTodayDate.js
+++ src/utils/getTodayDate.js
@@ -59,35 +59,35 @@ function dataOfTheWeek() {
     // If the current day is Monday
     case 1:
       // Add the next 5 days to the array
-      for (let i = 0; i < 6; i++) {
+      for (let i = 0; i < 5; i++) {
         dataOfWeek.push(nextDate(i));
       }
       break;
     // If the current day is Tuesday
     case 2:
       // Add the previous and next 5 days to the array
-      for (let i = -1; i < 5; i++) {
+      for (let i = -1; i < 4; i++) {
         dataOfWeek.push(nextDate(i));
       }
       break;
     // If the current day is Wednesday
     case 3:
       // Add the previous, current, and next 4 days to the array
-      for (let i = -2; i < 4; i++) {
+      for (let i = -2; i < 3; i++) {
         dataOfWeek.push(nextDate(i));
       }
       break;
     // If the current day is Thursday
     case 4:
       // Add the previous, current, and next 3 days to the array
-      for (let i = -3; i < 3; i++) {
+      for (let i = -3; i < 2; i++) {
         dataOfWeek.push(nextDate(i));
       }
       break;
     // If the current day is Friday
     case 5:
       // Add the previous, current, and next 2 days to the array
-      for (let i = -4; i < 2; i++) {
+      for (let i = -4; i < 1; i++) {
         dataOfWeek.push(nextDate(i));
       }
       break;
